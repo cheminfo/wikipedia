0e627b937c584770503b818002695c724de4ae04
Michaël Zasso <mic.besace@gmail.com>
2015-01-08 10:00:44 +0100

    parser : add publish and limit options

--- dump/wiki-parse.js
+++ dump/wiki-parse.js
@@ -2,7 +2,13 @@ var fs = require('fs'),
     util = require('./util'),
     ACT = require('./actelion'),
     Chemcalc = require('chemcalc'),
-    ProgressBar = require('progress');
+    ProgressBar = require('progress'),
+    program = require('commander');
+
+program
+    .option('-l, --limit <n>', 'Limit number of parsed pages', parseInt, 0)
+    .option('-p, --publish', 'Publish the final data file')
+    .parse(process.argv);
 
 var reg = /\| *smiles[12]? *= *([^}\n \|]*)/i;
 function getSmiles(content) {
@@ -17,6 +23,10 @@ var results = [],
     errors = [],
     notfound = [],
     length = pageList.length;
+
+if(program.limit) {
+    length = Math.min(program.limit, length);
+}
 
 console.log(length + ' pages to parse');
 
@@ -77,9 +87,15 @@ theResult.data = {
 theResult.query = {type: 'mol2d', value: ''};
 theResult.queryOptions = {searchMode: 'Substructure'};
 
-fs.writeFileSync('./data.json', JSON.stringify(theResult, null, 2));
-fs.writeFileSync('../site/src/json/data.json', JSON.stringify(theResult));
-
 console.log('Successfully extracted ' + results.length + ' molecules');
 console.log(errors.length + ' errors');
 console.log(notfound.length + ' not found');
+
+fs.writeFileSync('./data/data.json', JSON.stringify(theResult, null, 2));
+if (program.publish) {
+    if (program.limit) {
+        console.error('Cannot publish partial data');
+    } else {
+        fs.writeFileSync('../site/src/json/data.json', JSON.stringify(theResult));
+    }
+}
