adb59d515faa7c3935d111ebb58ab7984c96a05c
Michaël Zasso <mic.besace@gmail.com>
2015-01-15 15:00:31 +0100

    update bash script to auto-push new data

--- dump/all.sh
+++ dump/all.sh
@@ -1,6 +1,18 @@
 #!/bin/bash -e
 
+echo "Updating project files"
+git pull
+
+echo "Fetching data from Wikipedia"
+# get everything and generate data.json
 node wiki.js ids
 node wiki.js rev
 node wiki.js update
 node wiki.js parse -p
+
+# pull again to avoid conflicts, then commit the new data
+echo "Publishing data"
+git pull
+git add ../site/src/json/data.json
+git commit -m "update data"
+git push
