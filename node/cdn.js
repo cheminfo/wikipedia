b99e25c186b2082b3c13522166899f616f753a9f
Michaël Zasso <mic.besace@gmail.com>
2015-01-17 17:54:33 +0100

    automatic creation of cdn-related files

---
+++ node/cdn.js
@@ -0,0 +1,15 @@
+/*
+Automatically update the indexCDN.html and mainCDN.js files
+ */
+
+var config = require('./config.json');
+
+var fs = require('fs');
+
+var main = fs.readFileSync('../site/src/main.js', 'utf-8');
+var mainCDN = main.replace("baseUrl: 'lib/visualizer'", "baseUrl: 'http://www.lactame.com/visualizer/" + config.visualizer.cdn + "'");
+fs.writeFileSync('../site/src/mainCDN.js', mainCDN);
+
+var index = fs.readFileSync('../site/index.html', 'utf-8');
+var indexCDN = index.replace('<script data-main="src/main" src="lib/visualizer/components/requirejs/require.js">', '<script data-main="src/mainCDN" src="http://www.lactame.com/visualizer/' + config.visualizer.cdn + '/components/requirejs/require.js">');
+fs.writeFileSync('../site/indexCDN.html', indexCDN);
