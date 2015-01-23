22a20d99923695189987c423a5190ff3b941281f
Michaël Zasso <mic.besace@gmail.com>
2015-01-23 10:33:14 +0100

    update visualizer

---
+++ site/lib/visualizer/lib/loadingplot/loadingplot.js
@@ -0,0 +1,1 @@
+define(["./point","./springs","./svg"],function(a,b,c){return{SVGElement:a.SVGElement,Ellipse:a.Ellipse,Image:a.Image,Pie:a.Pie,SpringLabels:b,SVG:c}});
---
+++ site/lib/visualizer/modules/types/legacy/button_url/model.js
@@ -0,0 +1,1 @@
+define(["../../../default/defaultmodel"],function(a){function b(){}return b.prototype=$.extend(!0,{},a,{getValue:function(){return this.dataValue},getjPath:function(){return{}}}),b});
