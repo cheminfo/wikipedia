25141bf3ad0e065577eb5571652fe1be6b5a6018
moonayyur <jm_kias@esi.dz>
2022-10-31 14:28:17 +0100

    Add wikipedia article

--- src/components/StructureExplorer/Wikipedia.tsx
+++ src/components/StructureExplorer/Wikipedia.tsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from 'react';
+
 import SimpleTable from '../SimpleTable';
 
 function OpenWiki(): JSX.Element {
@@ -14,7 +16,28 @@ function OpenWiki(): JSX.Element {
 }
 
 function WikiPage(): JSX.Element {
-  return <div className="h-96">Some article</div>;
+  const [contents, setContents] = useState('');
+  const url =
+    // 'https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=revisions&rvprop=content&pageids=1365';
+    'https://en.wikipedia.org/w/api.php?action=query&origin=*&format=json&prop=extracts&pageids=1365';
+
+  useEffect(() => {
+    void fetch(url)
+      .then((response) => response.json())
+      .then((myJson) => {
+        // setContents(myJson.query.pages[1365].revisions[0]['*']);
+        setContents(myJson.query.pages[1365].extract);
+      });
+  }, []);
+
+  return (
+    <div className="p-5">
+      {
+        // eslint-disable-next-line react/no-danger
+        <div dangerouslySetInnerHTML={{ __html: contents }} />
+      }
+    </div>
+  );
 }
 
 export function Wikipedia(): JSX.Element {
