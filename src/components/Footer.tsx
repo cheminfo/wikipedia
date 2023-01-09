6f8d8dd740a2a1c6c42b56541852303417723dd5
moonayyur <jm_kias@esi.dz>
2023-01-09 20:56:00 +0100

    Add zakodium logo, fix errors close #48, close #52

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,31 +1,46 @@
-import actelion from '../../public/logo/actelion.png';
-import epfl from '../../public/logo/epfl.svg';
-import novartis from '../../public/logo/novartis.svg';
+/* eslint-disable import/no-absolute-path */
+
+import actelion from '/logo/actelion.png';
+import epfl from '/logo/epfl.svg';
+import novartis from '/logo/novartis.svg';
+import zakodium from '/logo/zakodium.svg';
+
+const logos = [
+  {
+    name: novartis,
+    link: 'https://www.novartis.com/',
+    className: 'w-36 lg:w-48',
+  },
+  {
+    name: epfl,
+    link: 'https://www.epfl.ch/en/',
+    className: 'w-28 lg:w-36',
+  },
+  {
+    name: actelion,
+    link: 'https://www.janssen.com/pulmonary-hypertension/',
+    className: 'w-32 lg:w-40',
+  },
+  {
+    name: zakodium,
+    link: 'https://www.zakodium.com/',
+    className: 'w-36 lg:w-44',
+  },
+];
 
 export function Footer(): JSX.Element {
   return (
-    <div className="flex flex-wrap items-center justify-center gap-x-24 gap-y-10 px-4 pb-12 pt-6 lg:gap-x-52">
-      <a
-        href="https://www.novartis.com/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={novartis} alt="novartis" className="w-36 lg:w-48" />
-      </a>
-      <a
-        href="https://www.epfl.ch/en/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={epfl} alt="epfl" className="w-28 lg:w-36" />
-      </a>
-      <a
-        href="https://www.janssen.com/pulmonary-hypertension/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <img src={actelion} alt="actelion" className="w-32 lg:w-40" />
-      </a>
+    <div className="flex flex-wrap items-end justify-center gap-x-16 gap-y-14 px-6 pb-12 pt-6 lg:gap-x-32">
+      {logos.map((logo) => (
+        <a
+          key={logo.name}
+          href={logo.link}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={logo.name} alt={logo.name} className={logo.className} />
+        </a>
+      ))}
     </div>
   );
 }
