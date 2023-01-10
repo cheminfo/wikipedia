93d3a5f59bea3a9cad9d1305132bef80d6d08479
moonayyur <jm_kias@esi.dz>
2023-01-10 18:44:14 +0100

    Update mw format closes #55

--- src/components/StructureExplorer/MoleculeInfo.tsx
+++ src/components/StructureExplorer/MoleculeInfo.tsx
@@ -13,23 +13,20 @@ export function MoleculeInfo({ mol }: MoleculeInfoProps): JSX.Element {
 
   return (
     <div
-      className="flex h-[217px] flex-col items-center justify-between overflow-hidden border py-1 text-xs font-bold hover:bg-lightgray"
+      className="flex h-[217px] flex-col items-center justify-between overflow-hidden border py-2 px-1 text-xs hover:bg-lightgray"
       onClick={() => setSelectedTitle(mol.code)}
     >
-      <div className="text-center">{mol.code}</div>
-      <div className="">
+      <div className="text-center font-bold">{mol.code}</div>
+      <div>
         <IdcodeSvgRenderer height={135} width={180} idcode={mol.actID.value} />
       </div>
-      <div className="">
-        <div className="flex space-x-3">
-          <div className="">MF:</div>
-          <div className="font-normal">
-            <MF mf={mol.mf.value} />
-          </div>
+      <div className="flex flex-wrap justify-center gap-x-1">
+        <div>
+          <MF mf={mol.mf.value} />
         </div>
-        <div className="flex space-x-2">
-          <div className="">MW:</div>
-          <div className="font-normal">{mol.mw}</div>
+        <div className="flex">
+          (<div className="mr-0.5 font-bold">mw:</div>
+          <div>{mol.mw.toFixed(2)}</div>)
         </div>
       </div>
     </div>
