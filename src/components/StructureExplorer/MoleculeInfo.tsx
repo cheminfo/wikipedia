136e26eea1865701c534f828b6ca9e732e2fce0c
moonayyur <jm_kias@esi.dz>
2023-01-13 20:17:53 +0100

    Add hyperlink to wikipedia for molecule on hover

--- src/components/StructureExplorer/MoleculeInfo.tsx
+++ src/components/StructureExplorer/MoleculeInfo.tsx
@@ -1,3 +1,6 @@
+import clsx from 'clsx';
+import { useState } from 'react';
+import { AiOutlineQuestionCircle } from 'react-icons/ai';
 import { MF } from 'react-mf';
 import { IdcodeSvgRenderer } from 'react-ocl';
 
@@ -10,13 +13,31 @@ interface MoleculeInfoProps {
 
 export function MoleculeInfo({ mol }: MoleculeInfoProps): JSX.Element {
   const { setSelectedTitle } = useIdContext();
+  const [showWikiBtn, setShowWikiBtn] = useState(false);
 
   return (
     <div
       className="flex h-[217px] flex-col items-center justify-between overflow-hidden border py-2 px-1 text-xs hover:bg-lightgray"
       onClick={() => setSelectedTitle(mol.code)}
+      onMouseEnter={() => setShowWikiBtn(true)}
+      onMouseLeave={() => setShowWikiBtn(false)}
     >
-      <div className="text-center font-bold">{mol.code}</div>
+      <div className="mx-5 text-center font-bold">{mol.code}</div>
+      <a
+        href={`https://en.wikipedia.org/wiki/${mol.code}`}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <AiOutlineQuestionCircle
+          className={clsx(
+            'absolute top-1.5 right-2 cursor-help text-xl text-darkblue',
+            {
+              'opacity-0': !showWikiBtn,
+              'transform transition-all duration-200 ease-in-out': showWikiBtn,
+            },
+          )}
+        />
+      </a>
       <div>
         <IdcodeSvgRenderer height={135} width={180} idcode={mol.actID.value} />
       </div>
