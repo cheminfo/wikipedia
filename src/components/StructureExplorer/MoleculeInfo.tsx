04a048b3b4bfd451bdd4a5a634de4e2cb431f5ad
Mounia <74436107+moonayyur@users.noreply.github.com>
2023-01-20 07:58:06 +0100

    Add bg color to the selected molecule cell (#64)

--- src/components/StructureExplorer/MoleculeInfo.tsx
+++ src/components/StructureExplorer/MoleculeInfo.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { MF } from 'react-mf';
 import { IdcodeSvgRenderer } from 'react-ocl';
 
@@ -9,12 +10,18 @@ interface MoleculeInfoProps {
 }
 
 export function MoleculeInfo({ mol }: MoleculeInfoProps): JSX.Element {
-  const { setSelectedTitle } = useIdContext();
+  const { selectedTitle, setSelectedTitle } = useIdContext();
 
   return (
     <button
       type="button"
-      className="flex h-[217px] w-full flex-col items-center justify-between overflow-hidden border py-2 px-1 text-xs hover:bg-lightgray"
+      className={clsx(
+        'flex h-[217px] w-full flex-col items-center justify-between overflow-hidden border py-2 px-1 text-xs',
+        {
+          'border-darkgray bg-darkgray': mol.code === selectedTitle,
+          'hover:bg-lightgray': mol.code !== selectedTitle,
+        },
+      )}
       onClick={() => setSelectedTitle(mol.code)}
     >
       <div className="text-center font-bold">{mol.code}</div>
