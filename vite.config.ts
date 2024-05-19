7b754085674dd48498adb482b7887ebd516cd6ec
Michaël Zasso <targos@protonmail.com>
2024-05-19 09:18:53 +0200

    chore: add build hook

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
 import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 
+const injectHead = process.env.BUILD_INJECT_HEAD;
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), ...(injectHead ? [htmlPlugin(injectHead)] : [])],
 });
+
+function htmlPlugin(injectHead: string) {
+  return {
+    name: 'html-transform',
+    transformIndexHtml(html: string) {
+      return html.replace('</head>', `${injectHead}</head>`);
+    },
+  };
+}
